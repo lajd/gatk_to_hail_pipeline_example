304bfb8f018ce6f0f58d295740c8b9d4e5cdd26c
Jon La <jon.ladavid@gmail.com>
2022-06-04 23:08:04 -0700

    Add reference genome arg when loading vcf as mt, fix bug

--- src/hail/utils.py
+++ src/hail/utils.py
@@ -8,10 +8,11 @@ except Exception:
     pass
 
 
-def vcf_path_to_mt(vcf_path: str, extension: str = '.mt') -> hl.MatrixTable:
-    mt_path = os.path.basename(vcf_path).split('.')[0] + extension
+def vcf_path_to_mt(vcf_path: str, extension: str = '.mt', reference_genome: str = 'GRCh37') -> hl.MatrixTable:
+    vcf_dir = os.path.dirname(vcf_path)
+    mt_path = os.path.join(vcf_dir, os.path.basename(vcf_path).split('.')[0] + extension)
     if not os.path.exists(mt_path):
-        mt = hl.import_vcf(vcf_path)
+        mt = hl.import_vcf(vcf_path, reference_genome=reference_genome)
         mt.write(mt_path)
     else:
         mt = hl.read_matrix_table(mt_path)
